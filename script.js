191062790eef51441398f2d5c348184d13312bce
Caleb Theil <86980706+ctheil@users.noreply.github.com>
2022-03-09 22:02:51 -0700

    added landing anima

--- script.js
+++ script.js
@@ -225,3 +225,18 @@ const currentNavFunc = (arrOne, arrTwo) => {
 currentNavFunc(currentNavArr, navLink);
 
 console.timeEnd("script");
+
+//landing anima
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(
+  ".big-text__icon",
+  { opacity: 0 },
+  { opacity: 1, duration: 1 },
+  "-=1"
+);
+tl.fromTo("#logo", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
